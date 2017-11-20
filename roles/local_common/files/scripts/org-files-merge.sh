b8fb7e903bb3718daa8a680fb79bc5647f9dc1b6
John Storey <john.storey@insureon.com>
2017-11-20 13:16:17 -0800

    Expanded and renamed org-files-merge.sh script.
    - Was not trying o pull from repo before merging local changes.
    - Changed name to be consistent with org-files-setup.sh

--- roles/local_common/files/scripts/org-files-merge.sh
+++ roles/local_common/files/scripts/org-files-merge.sh
@@ -7,13 +7,31 @@ WORKING_BRANCH="local-work"
 echo "Attempting to merge Org work and push to repository."
 
 if [[ -d $DIRECTORY ]]; then
+
+  # Commit current state.
+
   cd ~/Org
   TIMESTAMP=$(date +"%Y-%m-%d-%H:%M:%S")
   git commit -a -m "$TIMESTAMP"
+
+  # Update remote state.
+
+  git checkout master
+  git pull
+
+  # Rebase local work.
+
+  git checkout $WORKING_BRANCH
   git rebase master
+
+  # Merge into master and push.
+
   git checkout master
   git merge $WORKING_BRANCH
   git push
+
+  # Leave us ready to work.
+
   git checkout $WORKING_BRANCH
 
 else
